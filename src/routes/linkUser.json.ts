69b71dd7aecb41818b6dc4784df3966b9f7e5193
Jon Evans <jon@rezendi.com>
2021-01-08 11:46:39 -0800

    linkUser fix

--- src/routes/linkUser.json.ts
+++ src/routes/linkUser.json.ts
@@ -9,7 +9,7 @@ export async function post(req, res, next) {
       if (json.site=="github.com") {
         let username = json.result.additionalUserInfo.username;
         let token = json.result.credential.oauthAccessToken;
-        uid = req.session.slUser.uid || json.result.user.uid;
+        uid = req.session.sUser.uid || json.result.user.uid;
         // here's where you would save the user's username and OAuth token for subsequent use
         res.end(JSON.stringify({success:true}));
       }
@@ -26,9 +26,9 @@ export async function del(req, res, next) {
   });
   let json = req.body;
   try {
-    let idsIdx = req.session.slUser.identities.indexOf(json.site);
+    let idsIdx = req.session.sUser.identities.indexOf(json.site);
     if (idsIdx >= 0) {
-      req.session.slUser.identities.splice(idsIdx, 1);
+      req.session.sUser.identities.splice(idsIdx, 1);
     }
     res.end(JSON.stringify({success:true}));
   } catch(error) {
