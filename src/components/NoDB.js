8c8107a4ee853f90874082ad88963c2f09eef8f1
Jon Evans <jon@rezendi.com>
2021-02-16 14:49:05 -0800

    move delete to API layer too

--- src/components/NoDB.js
+++ src/components/NoDB.js
@@ -7,9 +7,15 @@ function slugize(str, locale) {
     return slugify (str, { lower: true, strict: true, locale: locale || 'en'} );
 }
 
+// args: auth, owner, repo, branch, prefix, title, sha, oldTitle, message, committer, locale='en'
 async function save(data, args) {
     try {
-        // auth, owner, repo, branch, path, title, sha, oldTitle, message, committer, locale='en'
+        let headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": `Basic ${base64.encode(`${args.owner}:${args.auth}`)}`
+        };
+
         let slug = slugize(args.title, args.locale );
         let path = `${args.prefix}/${slug}.yaml`; // hardcode 'examples' out of security paranoia
         let api_url = `https://api.github.com/repos/${args.owner}/${args.repo}/contents/${path}`;
@@ -18,8 +24,8 @@ async function save(data, args) {
         let toPut = {
             message: args.message || "NoDB update",
             content: base64.encode(yamlData),
+            branch: args.branch || "main",
             sha: args.sha,
-            branch: args.branch || "main"
         };
 
         // Get rid of SHA if we're doing a file rename.
@@ -27,16 +33,15 @@ async function save(data, args) {
             let originalSlug = slugize(args.originalTitle, args.locale );
 			doRename = originalSlug != slug;
 		}
+		if (doRename) {
+			delete toPut["sha"];
+		}
 
         // Get a SHA if we're doing what might be an update.
         if (!toPut.sha && !doRename && !args.guaranteedCreate) {
             let getResponse = await fetch(api_url, {
                 method: 'GET',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/vnd.github.v3+json",
-                    "Authorization": `Basic ${base64.encode(`${owner}:${args.auth}`)}`
-                },
+                headers: headers
             });
             let getJson = await getResponse.json();
             if (getJson.sha && getJson.sha.length > 0) {
@@ -44,17 +49,9 @@ async function save(data, args) {
             }
         }
 
-		if (doRename) {
-			delete toPut["sha"];
-		}
-
         let response = await fetch(api_url, {
             method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/vnd.github.v3+json",
-                "Authorization": `Basic ${base64.encode(`${owner}:${args.auth}`)}`
-            },
+            headers: headers,
             body: JSON.stringify(toPut)
         });
         let json = await response.json();
@@ -66,32 +63,74 @@ async function save(data, args) {
         // TODO: 1-commit renames https://medium.com/@obodley/renaming-a-file-using-the-git-api-fed1e6f04188
         let ghd = null;
         if (doRename) { 
-			let toDelete = {
-				message: "NoDB two-step file rename",
+            let ghd = del({
+                auth: args.auth,
+                owner: args.owner,
+                repo: args.repo,
+				branch: args.branch,
+                prefix: args.prefix,
+                title: args.originalTitle,
 				sha: args.sha,
-				branch: args.branch
-			};
-			let dSuffix = `lines/${path}/${originalSlug}.yaml`;
-			let dResponse = await fetch(`https://api.github.com/repos/${args.owner}/${args.repo}/contents/${dSuffix}`, {
-				method: 'DELETE',
-				headers: {
-					"Content-Type": "application/json",
-					"Accept": "application/vnd.github.v3+json",
-                    "Authorization": `Basic ${base64.encode(`${owner}:${args.auth}`)}`
-				},
-				body: JSON.stringify(toDelete)
-			});
-			ghd = await dResponse.json();
+                deleteEntireBranch: false,
+                message: "NoDB two-step file rename",
+            });
         }
 
-        return {success: success, gh:gh, ghd:ghd};
+        return { success: success, gh:gh, ghd:ghd };
 	} catch(error) {
-		return {success:false, args:args, error:error};
+		return { success:false, args:args, error:error };
 	}
 }
 
-async function del(data, args) {
+async function del(args) {
+	try {
+        let headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": `Basic ${base64.encode(`${args.owner}:${args.auth}`)}`
+        };
 
+        if (args.branch && args.deleteEntireBranch) {
+			let api_url = `https://api.github.com/repos/${args.owner}/${args.repo}/git/refs/${args.branch}`;
+			let response = await fetch(api_url, {
+				method: 'DELETE',
+				headers: headers
+			});
+			let gh = await response.json();
+			return {success:response.status==204, gh:gh};
+		}
+
+        // if wer'e here, we're deleting an individual file
+        let slug = slugize(args.title, args.locale );
+        let path = `${args.prefix}/${slug}.yaml`; // hardcode 'examples' out of security paranoia
+        let api_url = `https://api.github.com/repos/${args.owner}/${args.repo}/contents/${path}`;
+
+        let sha = args.sha;
+        // get the sha if needed
+        if (!sha) {
+            let getResponse = await fetch(api_url, {
+                method: 'GET',
+                headers: headers,
+            });
+            let getJson = await getResponse.json();
+            sha = getJson.sha;
+        }
+
+		let toDel = {
+			message: args.message,
+            branch: args.branch || "main",
+			sha: sha,
+		};
+		let response = await fetch(api_url, {
+			method: 'DELETE',
+			headers: headers,
+			body: JSON.stringify(toDel)
+		});
+		let gh = await response.json();
+        return { success: success, gh:gh };
+	} catch(error) {
+		return { success:false, args:args, error:error };
+	}
 }
 
 
--- src/routes/save.json.ts
+++ src/routes/save.json.ts
@@ -16,11 +16,11 @@ export async function post(req, res, next) {
 		auth: process.env.GITHUB_TOKEN,
 		owner: process.env.GITHUB_ACCOUNT,
 		repo: process.env.GITHUB_REPO,
-		prefix: prefix,
 		branch: data.branch,
+		prefix: prefix,
+		title: data.title,
 		sha: data.sha,
 		message: "NoDB update",
-		title: data.title,
 	};
 	let retval = NoDB.save(data, args);
 	res.end(JSON.stringify(retval));
@@ -28,48 +28,20 @@ export async function post(req, res, next) {
 
 export async function del(req, res, next) {
 	console.log("deleting");
-	res.writeHead(200, {
-		'Content-Type': 'application/json'
-	});
+	let user = req.session.sUser;
+	// Hardcode "examples" in he path out of mild security paranoia.
+	let prefix = "examples/" + user.username ? user.username : util.hash8(user.email);
 	let data = req.body;
-	try {
-		let owner = process.env.GITHUB_ACCOUNT;
-		let repo = process.env.GITHUB_REPO;
-		let user = req.session.sUser;
-		let pathPrefix = user.username ? user.username : util.hash8(user.email)
-		let path = `examples/${pathPrefix}/${data.slug}.yaml`;
-		let api_url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
-
-		// get the sha
-		let getResponse = await fetch(api_url, {
-			method: 'GET',
-			headers: {
-				"Content-Type": "application/json",
-				"Accept": "application/vnd.github.v3+json",
-				"Authorization": `Basic ${base64.encode(`${owner}:${process.env.GITHUB_TOKEN}`)}`
-			},
-		});
-		let getJson = await getResponse.json();
-		data.sha = getJson.sha;
-
-		let toDel = {
-			message: "NoDB delete",
-			sha: data.sha,
-		};
-		let response = await fetch(api_url, {
-			method: 'DELETE',
-			headers: {
-				"Content-Type": "application/json",
-				"Accept": "application/vnd.github.v3+json",
-				"Authorization": `Basic ${base64.encode(`${owner}:${process.env.GITHUB_TOKEN}`)}`
-			},
-			body: JSON.stringify(toDel)
-		});
-		let json = await response.json();
-		console.log("deleted", json);
-		res.end(JSON.stringify({json, ...{ success: true}}));
-	} catch(error) {
-		console.log("save error", error);
-		res.end(JSON.stringify({success:false, data:data, error:error}));
-	}
+	let args = {
+		auth: process.env.GITHUB_TOKEN,
+		owner: process.env.GITHUB_ACCOUNT,
+		repo: process.env.GITHUB_REPO,
+		branch: data.branch,
+		prefix: prefix,
+		title: data.title,
+		sha: data.sha,
+		message: "NoDB delete",
+	};
+	let retval = NoDB.del(args);
+	res.end(JSON.stringify(retval));
 }
